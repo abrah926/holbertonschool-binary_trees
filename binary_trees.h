34f62784447a11fd7d503eac8575c39b2ab4044f
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-08-01 13:10:48 -0400

    updated my prototypes

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,10 @@
 
 #include <stdlib.h>
 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *);
+
+
 /**
  * struct binary_tree_s - Binary tree node
  *
