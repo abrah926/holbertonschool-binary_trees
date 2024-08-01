b2f693852152a9f56830c713a36652245d7297ba
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-08-01 13:18:37 -0400

    func that prints out nodes created

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,7 +7,7 @@
  * @value: Value to put in the new node
  * Return: Pointer to the new node, or NULL on failure
  */
-binary_tree_t *binary_tree_node(binary_tree_t * parent,
+binary_tree_t *binary_tree_node(binary_tree_t *parent,
 	int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
