ea43bb1c4634524931e78fcb72eb52bcd48ca59d
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-08-01 14:57:17 -0400

    funct that frees the tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ *
+ * Description: This function uses a post-order traversal to delete the tree,
+ *              ensuring that each node is deleted only after all its children
+ *              have been deleted.
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
